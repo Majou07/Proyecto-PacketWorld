35e37c3450327e9f321f5732648f6aaad0d3a6c2
Maria Jose <zS22016128@estudiantes.uv.mx>
2025-12-14 15:54:01 -0600

    actualización Majo

--- ClienteEscritorio/src/clienteescritorio/ClienteEscritorio.java
+++ ClienteEscritorio/src/clienteescritorio/ClienteEscritorio.java
@@ -1,9 +1,12 @@
 
 package clienteescritorio;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -13,29 +16,19 @@ public class ClienteEscritorio extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        try{
+            Parent vista = 
+                FXMLLoader.load(getClass().getResource("FXMLInicioSesion.fxml"));
+            Scene escenaLogin = new Scene(vista);
+            primaryStage.setScene(escenaLogin);
+            primaryStage.setTitle("Inicio de Sesión - Packet World");
+            primaryStage.show();
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
     }
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         launch(args);
     }
