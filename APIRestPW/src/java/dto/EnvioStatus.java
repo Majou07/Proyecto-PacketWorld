9d37b6fd9b8e578a79aa30397ae5bb2f76f2cd13
Maria Jose <147551384+Majou07@users.noreply.github.com>
2026-01-05 01:47:44 -0600

    actualización Majo

---
+++ APIRestPW/src/java/dto/EnvioStatus.java
@@ -0,0 +1,40 @@
+package dto;
+
+
+public class EnvioStatus {
+    private String estatus;
+    private String comentario;
+    private int idColaborador;
+
+    public EnvioStatus() {
+    }
+
+    public String getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public int getIdColaborador() {
+        return idColaborador;
+    }
+
+    public void setIdColaborador(int idColaborador) {
+        this.idColaborador = idColaborador;
+    }
+    
+
+    
+    
+    
+}
