c4b8453d85c48bdcc61b7f57e0d93134428680c5
Maria Jose <147551384+Majou07@users.noreply.github.com>
2026-01-05 02:11:10 -0600

    Cambio de nombres

--- APIRestPW/src/java/dto/EnvioStatus.java
+++ APIRestPW/src/java/dto/EnvioStatus.java
@@ -2,19 +2,33 @@ package dto;
 
 
 public class EnvioStatus {
-    private String estatus;
-    private String comentario;
+    private int idEstatusEnvio;
     private int idColaborador;
+    private String comentario;
 
     public EnvioStatus() {
     }
 
-    public String getEstatus() {
-        return estatus;
+    public EnvioStatus(int idEstatusEnvio, int idColaborador, String comentario) {
+        this.idEstatusEnvio = idEstatusEnvio;
+        this.idColaborador = idColaborador;
+        this.comentario = comentario;
     }
 
-    public void setEstatus(String estatus) {
-        this.estatus = estatus;
+    public int getIdEstatusEnvio() {
+        return idEstatusEnvio;
+    }
+
+    public void setIdEstatusEnvio(int idEstatusEnvio) {
+        this.idEstatusEnvio = idEstatusEnvio;
+    }
+
+    public int getIdColaborador() {
+        return idColaborador;
+    }
+
+    public void setIdColaborador(int idColaborador) {
+        this.idColaborador = idColaborador;
     }
 
     public String getComentario() {
@@ -25,12 +39,5 @@ public class EnvioStatus {
         this.comentario = comentario;
     }
 
-    public int getIdColaborador() {
-        return idColaborador;
-    }
-
-    public void setIdColaborador(int idColaborador) {
-        this.idColaborador = idColaborador;
-    }
-    
+ 
 }
