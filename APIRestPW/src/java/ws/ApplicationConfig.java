c5ec4d1be743734ffcff28c9cc14a73e721420d2
Maria Jose <147551384+Majou07@users.noreply.github.com>
2026-01-12 04:06:19 -0600

    Se coloca en automatico direccionws

--- APIRestPW/src/java/ws/ApplicationConfig.java
+++ APIRestPW/src/java/ws/ApplicationConfig.java
@@ -19,6 +19,7 @@ public class ApplicationConfig extends Application {
         resources.add(ws.CatalogoCPWS.class);
         resources.add(ws.ClienteWS.class);
         resources.add(ws.ColaboradorWS.class);
+        resources.add(ws.DireccionWS.class);
         resources.add(ws.EnvioWS.class);
         resources.add(ws.PaqueteWS.class);
         resources.add(ws.SucursalWS.class);
